d1ebb492238927345135530c13ec08734c332346
Evan Linde <elinde@okstate.edu>
2018-08-24 15:26:42 -0500

    add Social Sciences Data Carpentry lesson

--- dc_lessons.sh
+++ dc_lessons.sh
@@ -10,4 +10,11 @@ cd "${LESSON}"
 wget -O 1314459.zip https://ndownloader.figshare.com/articles/1314459/versions/6
 unzip 1314459.zip
 
-
+# Download files for the Social Science lessons
+LESSON="dc-socialsci"
+cd ~/Desktop
+[ -d "${LESSON}" ] && rm -rf "${LESSON}"
+mkdir "${LESSON}"
+cd "${LESSON}"
+wget -O 6262019.zip https://ndownloader.figshare.com/articles/6262019/versions/4
+unzip 6262019.zip
