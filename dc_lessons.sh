78c6acaf818e00b478fd39aa184c96842eab774a
Evan Linde <elinde@okstate.edu>
2018-08-31 14:44:43 -0500

    add Data Carpentr Geospatial lesson (commented out)

--- dc_lessons.sh
+++ dc_lessons.sh
@@ -18,3 +18,15 @@ mkdir "${LESSON}"
 cd "${LESSON}"
 wget -O 6262019.zip https://ndownloader.figshare.com/articles/6262019/versions/4
 unzip 6262019.zip
+
+## Download files for the Geospatial lessons
+#LESSON="dc-geospatial"
+#cd ~/Desktop
+#[ -d "${LESSON}" ] && rm -rf "${LESSON}"
+#mkdir "${LESSON}"
+#cd "${LESSON}"
+#wget -O 2009586.zip https://ndownloader.figshare.com/articles/2009586/versions/10
+#unzip 2009586.zip
+#for f in NEON-DS-*.zip; do unzip $f; done
+#rm NEON-DS-*.zip
+
